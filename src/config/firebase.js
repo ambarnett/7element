2e4e3576b7733e2674b3ac482b2357eefa8b8ba3
ambarnett <andy.barnett09@gmail.com>
2023-04-27 11:43:38 -0500

    initial set up commit

---
+++ src/config/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "element-b5023.firebaseapp.com",
+    projectId: "element-b5023",
+    storageBucket: "element-b5023.appspot.com",
+    messagingSenderId: "635580726857",
+    appId: "1:635580726857:web:345516b5e723d565378998",
+    measurementId: "G-GMQQ62MFVQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider(app);
+export const db = getFirestore(app);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'; // this import makes react-bootstrap work throughout the code
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
